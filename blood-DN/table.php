b698933371621f1bd6da57ff672c0d05f56d2e5d
Rashed Alqhtani <rashed.bq5@gmail.com>
2021-10-16 18:32:06 +0300

    my commit with VS 2

--- blood-DN/table.php
+++ blood-DN/table.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
+<title>تبرع بالدم | طلبات الدم</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
